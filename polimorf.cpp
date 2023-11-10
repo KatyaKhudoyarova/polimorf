c794cc013ffc36a06a7d4e6fcbb03e8760e4ace2
katana <katana@DESKTOP-DJ237UF>
2023-11-10 18:44:05 +0200

    Добавьте файлы проекта.

---
+++ polimorf.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    virtual void tree() const = 0;
+        
+    
+};
+
+class FirstTree: public Base {
+    virtual void tree()const override {
+        cout <<" "<<"/ \\"<<endl;
+        cout << "// \\\\" << endl;
+    }
+};
+
+class SecondTree : public Base {
+    virtual void tree()const override {
+        cout << " " << "/\\" << endl;
+        cout << "/**\\" << endl;
+    }
+};
+
+    class ThirdTree : public Base {
+        virtual void tree()const override {
+            cout << " " << " " << "/\\" << endl;
+            cout << "/ ++ \\" << endl;
+        }
+        };
+
+        int main() {
+            const int trees = 3;
+            Base* array[trees] = { new FirstTree(), new SecondTree(), new ThirdTree() };
+
+            
+
+            for (auto tree: array) {
+                
+                tree->tree();
+                cout <<endl;
+            }
+            for (auto tree: array) {
+                delete tree;
+            }
+            return 0;
+        }
+   
